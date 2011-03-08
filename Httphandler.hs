1052908e49bd93399403516d832c927660cf5d5c
Uriel Corfa <uriel@corfa.fr>
2011-03-08 18:58:12 +0100

    Added configuration

--- Httphandler.hs
+++ Httphandler.hs
@@ -6,6 +6,8 @@ module Httphandler (
     import qualified Network.Socket.Internal as NSI
     import Text.ParserCombinators.Parsec
     import qualified Control.Exception as C (catch)
+    import Control.Monad.Reader
+    import HttpConfig
 
     data HttpRequest = HttpRequest {
           reqmethod :: String,
@@ -88,21 +90,26 @@ module Httphandler (
     instance Show HttpReply where
         show = httpReplyToString
 
-    generateResponse :: HttpRequest -> Int -> (NSI.HostAddress, NSI.PortNumber) -> IO HttpReply
-    generateResponse q _ _ = Prelude.catch (do
-      let file = reqressource q
+    generateResponse :: String -> HttpRequest -> Int -> (NSI.HostAddress, NSI.PortNumber) -> IO HttpReply
+    generateResponse prependPath q _ _ = Prelude.catch (do
+      let file = prependPath ++ reqressource q
       fileh <- Prelude.catch (openFile file ReadMode) (\_ -> ioError $ userError "404 Not Found")
       s <- hGetContents fileh
-      putStrLn s
-      hClose fileh
+--      hClose fileh
       return $ HttpReply (reqversion q) 200 "OK" [("server", "haskttpd")] s
                                    ) $ \_ ->
                                     return $ HttpReply (reqversion q) 404 "Not Found" [("server", "haskttpd")] "lolilol 404"
 
+    handle' :: Handle -> Int -> (NSI.HostAddress, NSI.PortNumber) -> ReaderT Config IO ()
+    handle' h cid connxInfos = do
+      conf <- ask
+      let prependPath = getValueOrEmpty conf "DocumentRoot"
+      req <- liftIO $ parseRequestFromStream h
+      liftIO $ putStrLn (show req)
+      resp <- liftIO $ generateResponse prependPath req cid connxInfos
+      liftIO $ putStrLn (show resp)
+      liftIO $ hPutStr h (show resp)
+
     handle :: Handle -> Int -> (NSI.HostAddress, NSI.PortNumber) -> IO ()
     handle h cid connxInfos = do
-      req <- parseRequestFromStream h
-      putStrLn (show req)
-      resp <- generateResponse req cid connxInfos
-      putStrLn (show resp)
-      hPutStr h (show resp)
+      runReaderT (handle' h cid connxInfos) $ Config [("DocumentRoot", "/home/korfuri/")]
